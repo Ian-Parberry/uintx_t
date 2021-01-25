8cdde6777ec6a431da43c5ea11f605744da86895
Ian Parberry <Ian-Parberry@users.noreply.github.com>
2021-01-25 10:43:59 -0600

    Refresh docs for Check program.

--- docs/html3/search/all_7.js
+++ docs/html3/search/all_7.js
@@ -3,7 +3,7 @@ var searchData=
   ['m_5fenodetype',['m_eNodeType',['../class_c_node.html#a01bdf285384059b50627d3cbf9cd0988',1,'CNode']]],
   ['m_5feoperator',['m_eOperator',['../class_c_node.html#a14cc04c622d34d83169dcedfb3921bb8',1,'CNode']]],
   ['m_5fesymbol',['m_eSymbol',['../class_c_lex.html#a9b5c3350d5d3d7eb753e5e86a7bdce06',1,'CLex']]],
-  ['m_5fmaperror',['m_mapError',['../class_c_parser.html#ad013e92e314ff64b467a742a279e8ff0',1,'CParser']]],
+  ['m_5fmaperror',['m_mapError',['../class_c_parser.html#a4d1f873c42450b0ea582bad146769800',1,'CParser']]],
   ['m_5fncurchar',['m_nCurChar',['../class_c_lex.html#a3d98eccc6dc22f2fae51a9ba87ad9324',1,'CLex']]],
   ['m_5fnnumber',['m_nNumber',['../class_c_lex.html#a20da8fbf0a20b8b405d25b0dfa097838',1,'CLex']]],
   ['m_5fnstrlen',['m_nStrLen',['../class_c_lex.html#a32736e04d0cc0e159f04598be64d0044',1,'CLex']]],
@@ -12,8 +12,9 @@ var searchData=
   ['m_5fpleftchild',['m_pLeftChild',['../class_c_node.html#a6e883b564c1f09c8aed3433f21e7dd72',1,'CNode']]],
   ['m_5fprightchild',['m_pRightChild',['../class_c_node.html#a3552abeab945b5c4b97404fe03a21594',1,'CNode']]],
   ['m_5fsetfunctions',['m_setFunctions',['../class_c_lex.html#a6fe92174dfed9707f7a1670388b311e1',1,'CLex']]],
+  ['m_5fstdidentifierstack',['m_stdIdentifierStack',['../class_c_lex.html#a363fd48a6fabaabe490ab1314ef0b323',1,'CLex']]],
   ['m_5fstrbuffer',['m_strBuffer',['../class_c_lex.html#a878b354725b0d548550d329eacbaaefc',1,'CLex']]],
-  ['m_5fstridentifier',['m_strIdentifier',['../class_c_node.html#aaa1e562de047690215625d2a3bf4339a',1,'CNode::m_strIdentifier()'],['../class_c_lex.html#a6c175ebf2c52f67adb1a2e7769d169bf',1,'CLex::m_strIdentifier()']]],
+  ['m_5fstridentifier',['m_strIdentifier',['../class_c_node.html#aaa1e562de047690215625d2a3bf4339a',1,'CNode']]],
   ['main',['main',['../main_8cpp.html#ae66f6b31b5ad750f1fe042a706a4e3d4',1,'main.cpp']]],
   ['main_2ecpp',['main.cpp',['../main_8cpp.html',1,'']]]
 ];
